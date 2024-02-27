55614ebff4cdcccf8efd61f1a6f85051bc810aaa
kashyap <kashyaprathod03@gmail.com>
2024-02-27 15:40:00 +0530

    atlas connection

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,8 @@
 const mongoose=require('mongoose');
 
 mongoose.set('strictQuery',false);
-const db = mongoose.connect("mongodb://127.0.0.1:27017/dashboard").then(()=>{console.log("database connection successfull")}).catch((err)=>{console.log(err)}); 
+const db = mongoose.connect("mongodb+srv://kashyaprathod03:cms@cluster0.wctzuup.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/dashboard").then(()=>{console.log("database connection successfull")}).catch((err)=>{console.log(err)}); 
 
 module.exports = db
+
+// const db = mongoose.connect("mongodb://127.0.0.1:27017/dashboard").then(()=>{console.log("database connection successfull")}).catch((err)=>{console.log(err)}); 
